c21b3329f09aee73afd73095bf8c2a03dac2afd4
Bacon-404 <www.mefagan02@gmail.com>
2021-01-21 20:18:55 -0500

    Update Rational.java

--- RationalProject/src/Rational.java
+++ RationalProject/src/Rational.java
@@ -5,9 +5,28 @@
  * @author Christian Fagan
  *
  */
+
+import InvalidDenominatorException;
+
 public class Rational {
 	
-	public static void main(String[] args) {
+	int numerator = 0;
+	int denominator = 0;
+	
+	/** 
+	 * 
+	 * @param a The value that the numerator will be set to.
+	 */
+	public Rational(int num, int den) {
+		numerator = num;
+		if (den <= -1)
+			throw InvalidDenominatorException;
+		denominator = den;
+	}
+	
+	public Rational(String num, String den) {
 		
 	}
+
+		
 }
