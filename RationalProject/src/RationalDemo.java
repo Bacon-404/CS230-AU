a942b9a4404606ee9ea5e3223d9d680b79e0e423
Bacon-404 <45476905+Bacon-404@users.noreply.github.com>
2021-02-01 11:36:05 -0500

    Update RationalDemo.java

--- RationalProject/src/RationalDemo.java
+++ RationalProject/src/RationalDemo.java
@@ -1,9 +1,24 @@
 
 public class RationalDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws InvalidDenominatorException {
+		Rational demo1 = new Rational(1,12);
+		
+		demo1.toString();
+		
+		String str1 = new String("-3");
+		String str2 = new String("12");
+		
+		Rational demo2 = new Rational(str1, str2);
+		
+		demo2.toString();
+		
+		String str3 = new String("1");
+		String str4 = new String("3");
+		
+		Rational demo3 = new Rational(str3, str4);
+		
+		demo3.toString();
 	}
 
 }
