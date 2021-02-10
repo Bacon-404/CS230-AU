ff0d9183ef71de12d42073de3c2fc254a3c878aa
Bacon-404 <www.mefagan02@gmail.com>
2021-02-10 01:28:45 -0500

    Update RegularPentagon.java

--- PentagonProject/src/RegularPentagon.java
+++ PentagonProject/src/RegularPentagon.java
@@ -1,6 +1,33 @@
 import ch02.figures.FigureInterface;
 
 public class RegularPentagon implements FigureInterface {
-	private int side;
+	private double side;
 	
+	public RegularPentagon(double s) {
+		
+	}
+	
+	public RegularPentagon(RegularPentagon pentagon) {
+		
+	}
+	
+	public double area() {
+		
+	}
+	
+	public double perimeter() {
+		
+	}
+	
+	public boolean equals(RegularPentagon pentagon) {
+		
+	}
+	
+	public ReqularPentagon copy() {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
