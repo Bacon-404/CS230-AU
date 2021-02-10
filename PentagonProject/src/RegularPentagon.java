1a1a2637a53f4a5116df550427c78a05545602cd
Bacon-404 <45476905+Bacon-404@users.noreply.github.com>
2021-02-10 11:45:44 -0500

    Start of branch

--- PentagonProject/src/RegularPentagon.java
+++ PentagonProject/src/RegularPentagon.java
@@ -1,4 +1,36 @@
+import ch02.figures.FigureInterface;
 
-public class RegularPentagon {
-
+public class RegularPentagon implements FigureInterface {
+	
+	private double side;
+	
+	public RegularPentagon(double s) {
+		
+	}
+	
+	public RegularPentagon(RegularPentagon pentagon) {
+		
+	}
+	
+	public double perimeter() {
+		double area;
+		
+		area = 0.25 Math.sqrt(5))Math.pow(side, 2);
+	}
+	
+	public double area() {
+		
+	}
+	
+	public boolean equals(RegularPentagon pentagon) {
+		
+	}
+	
+	public RegularPentagon copy() {
+		
+	}
+	
+	public String toString() {
+		
+	}
 }
