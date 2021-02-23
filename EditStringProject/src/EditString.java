2acf641ec9d18ae7a988f91b0e0782756648ec67
Bacon-404 <www.mefagan02@gmail.com>
2021-02-22 23:53:40 -0500

    Update EditString.java

--- EditStringProject/src/EditString.java
+++ EditStringProject/src/EditString.java
@@ -1,13 +1,60 @@
 
 import ch02.stacks.*;
+import java.util.Scanner;
 
 public class EditString {
 
 	
 	public static void main(String [] args) {
 		ArrayBoundedStack<String> mainStack = new ArrayBoundedStack<String>();
+		
 	
-		while (mainStack.isEmpty() != true)	 {
+		Scanner inputKeyboard = new Scanner(System.in);
+		System.out.print("Please input a string to edit.");
+		
+		String mainString = inputKeyboard.nextLine();
+		
+		mainStack.push(mainString);
+		System.out.println();
+		String orderString = inputKeyboard.nextLine();
+		while (!(orderString.equals("X")))	 {
+			
+			if (orderString.equals("U")) {
+				mainString = mainStack.peek().toUpperCase();
+				mainStack.push(mainString);
+			}
+			
+			if (orderString.equals("L")) {
+				mainString = mainStack.peek().toLowerCase();
+				mainStack.push(mainString);
+			}
+			
+			if (orderString.equals("A")) {
+				System.out.print("Please input where you "
+						+ "would like to change a character");
+				int pos = inputKeyboard.nextInt();
+				String replace = inputKeyboard.nextLine();
+				
+			}
+			
+			if (orderString.equals("C")) {
+				System.out.print("Please type out the string you "
+						+ "would like to add and press enter");
+				String conString = inputKeyboard.nextLine();
+				mainString = mainString + conString;
+				
+				System.out.print("Your changes have been added.");
+				mainStack.push(mainString);
+				
+			}
+			
+			if (orderString.equals("R")) {
+				
+			}
+			
+			else if (orderString.equals("Z")) {
+				mainStack.pop();
+			}
 			
 		}
 	
