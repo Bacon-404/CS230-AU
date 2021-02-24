43e999797aa54f9cf0c810cdb3624fce6e6fbd7c
Bacon-404 <45476905+Bacon-404@users.noreply.github.com>
2021-02-24 11:35:17 -0500

    Started Project 4

---
+++ RecursiveEvensProject/src/RecursiveEvens.java
@@ -0,0 +1,37 @@
+import support.LLNode;
+import java.util.Scanner;
+
+public class RecursiveEvens {
+	
+	public static void main(String [] args) {
+		LLNode<String> numList = new LLNode<String>(null);
+		
+		LLNode<String> newNode = new LLNode<String>(null);
+		
+		numList.setLink(newNode);
+		instruc();
+		Scanner inputKey = new Scanner(System.in);
+		
+		while(inputKey.hasNextInt()) {
+			newNode.setLink(new LLNode<String>(inputKey.nextLine()));
+			newNode = newNode.getLink();
+			if (inputKey.nextLine() == "0") {
+				break;
+			}
+		}
+		
+		
+	}
+	
+	public static void numEvens() {
+		
+	}
+	
+	
+	public static void instruc() {
+		System.out.println("Hello, this is a program about"
+				+ " finding even numbers");
+		System.out.println("We will need to collect numbers from you "
+				+ "to find the evens in the set");
+	}
+}
